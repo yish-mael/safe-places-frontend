954e10c3e7b961eb425e8b83d2c13d576e95ba0c
Tolu Akinnubi <tolu.akinnubi@yahoo.com>
2023-02-04 02:50:34 +0100

    feat: add map and search ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,67 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Map from 'react-map-gl'
+import { useEffect, useRef, useState } from 'react'
+import mapbox from 'mapbox-gl'
+
+mapbox.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
 function App() {
 
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lon, setLon] = useState(-95.3698);
+  const [lat, setLat] = useState(29.7604);
+  const [zoom, setZoom] = useState(10);
+  const [details, setDetails] = useState({name: "Huston"});
+
+  async function generateMap(map, mapContainer, lon, lat, zoom, details) {
+
+    map.current = new mapbox.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [lon, lat],
+        zoom: zoom
+    });
+
+    const popup = new mapbox.Popup().setHTML("<b>"+ details.name + "</b>");
+    new mapbox.Marker({ "color": "#BB2D3A" }).setLngLat([lon, lat]).addTo(map.current).setPopup(popup);
+    return () => map.current.remove();
+
+  }
+
+
+  useEffect(() => {
+    generateMap(map, mapContainer, lon, lat, zoom, details);
+  }, [lon, lat, zoom, details]);
+
+ 
   return (
     <div className="App">
-        <h1>Safe Places</h1>
+        <div ref={mapContainer} className="map-box"></div>
 
-        <Map
-          initialViewState={{
-            longitude: -100,
-            latitude: 40,
-            zoom: 3.5
-          }}
-          style={{width: 600, height: 400}}
-          mapStyle="mapbox://styles/mapbox/streets-v9"
-          mapboxAccessToken='' 
-        />
+        <div className='top-bar'>
 
+          <form class="row row-cols-lg-auto g-3 align-items-center">
 
+            <div class="col-12">
+              <label class="visually-hidden" for="inlineFormInputGroupUsername">Search</label>
+              <input type="text" class="form-control form-control-sm" placeholder="Search..." />
+              
+            </div>
+
+            <div class="col-12">
+              <label class="visually-hidden" for="inlineFormSelectPref">Radius</label>
+              <select class="form-select form-select-sm" id="inlineFormSelectPref">
+                <option value="0.1">0.1 Miles</option>
+                <option value="0.5">0.5 Miles</option>
+                <option value="5">5 Miles</option>
+              </select>
+            </div>
+            
+            <div class="col-12">
+              <button type="submit" class="btn btn-sm px-4 btn-danger rounded-pill"><b>Submit</b></button>
+            </div>
+          </form>
+
+        </div>
     </div>
   )
 }
