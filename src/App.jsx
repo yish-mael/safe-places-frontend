ee60fbd6101482ea3dc9c57238a5f4faad65cfaf
Tolu Akinnubi <tolu.akinnubi@yahoo.com>
2023-02-06 16:22:24 +0100

    feat: add popup results

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import 'mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
 import { useCallback, useEffect, useRef, useState } from 'react'
 import mapbox from 'mapbox-gl'
 import MapboxGeocoder from 'mapbox-gl-geocoder';
+import axios from 'axios';
 
 mapbox.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
@@ -16,7 +17,53 @@ function App() {
 
 
   async function calculateRisk(search){
-    
+
+    try{
+      const harrisResponse = await axios.get("http://127.0.0.1:8000/api/data");
+      const busyResponse = await axios.get("http://127.0.0.1:8000/api/busy?address="+search.address);
+      // console.log(search);
+      // console.log(harrisResponse.data.data.features);
+      let activeCovid = 0;
+      let population = 0;
+      let covidDeaths = 0;
+      let safety = "";
+      
+      const d = new Date();
+      const day = d.getDay();
+      const hours = d.getHours();
+      const harrisFeatures = harrisResponse.data.data.features;
+
+      for(let i = 0; i < harrisFeatures.length; i++ )
+      {
+        if(harrisFeatures[i].properties.ZIP == search.zip)
+        {
+          activeCovid = harrisFeatures[i].properties.ActiveCases;
+          population = harrisFeatures[i].properties.TotalPop;
+          covidDeaths = harrisFeatures[i].properties.Death;
+
+          let population20 = population/20;
+          let result = (activeCovid/population20) * 100;
+
+          if(result < 6.7) safety = ` <span class='bg-success text-white px-3 py-1 fw-bold rounded-pill'>Low Risk</span>`; 
+          else if(result > 13.3) safety = ` <span class='bg-danger text-white px-3 py-1 fw-bold rounded-pill'>High Risk</span>`;
+          else safety = ` <span class='bg-warning text-white px-3 py-1 fw-bold rounded-pill'>Medium Risk</span>`;
+        }
+      }
+
+      
+      
+      return {
+        busy: busyResponse?.data?.populartimes[day-1]?.data[hours],
+        covid: activeCovid,
+        population: population,
+        deaths: covidDeaths,
+        safety: safety,
+      }
+      //setBusyStatus(busyResponse?.data?.populartimes[day-1]?.data[hours])
+    }catch(e){
+      console.log(e);
+    }
+
   }
 
   async function generateMap( lon, lat, zoom, details) {
@@ -56,13 +103,28 @@ function App() {
         ));
           
     });
-    
-    geocoder.on('result', (event) => {
+
+    geocoder.on('result', async(event) => {
+      const calcResult = await calculateRisk({address: event?.result?.place_name, zip: event.result.context[0].text});
       map.getSource('single-point').setData(event.result.geometry);
-      console.log("hello: ", event.result)
+      // console.log("hello: ", event.result)
 
       const popup = new mapbox.Popup({ closeOnClick: false })
-                    .setHTML("<div><span class='bg-danger text-white px-3 py-1 fw-bold rounded-pill'>High Risk</span><p><h6>"+event.result.text+"</h6>"+ event.result.place_name + "</p></div>")
+                    .setHTML(`<div>
+                               ${calcResult.safety}
+                                <p>
+                                  <h6>${event.result.text}</h6>
+                                  ${event.result.place_name}
+                                  <br />
+                                  <span style='font-size: 13px;'><b>Population: </b> ${calcResult.population}</span>
+                                  <br />
+                                  <span style='font-size: 13px;'><b>Busy:</b> ${calcResult.busy}</b>% relative to peak period.</span>
+                                  <br />
+                                  <span style='font-size: 13px;'><b>Active Covid: </b> ${calcResult.covid}</span>
+                                  <br />
+                                  <span style='font-size: 13px;'><b>Covid Deaths: </b> ${calcResult.deaths}</span>
+                                </p>
+                              </div>`)
                     .addTo(map);
       new mapbox.Marker().setLngLat(event.result.center).addTo(map).setPopup(popup);
     });
